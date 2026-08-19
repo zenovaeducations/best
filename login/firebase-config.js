bf55c76dee275b8e18cd4e371918f18e412e2802
zenovaeducations <zenovaeducations@gmail.com>
2026-08-19 10:35:48 +0530

    Add Firebase configuration for the project

---
+++ login/firebase-config.js
@@ -0,0 +1,23 @@
+// Firebase configuration for BEST website
+// Keep this file separate from login.js
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAxG3Qhxj2hYQqQfXTB8MFR9jaL9bu3KiY",
+
+    authDomain: "best-6dc7e.firebaseapp.com",
+
+    projectId: "best-6dc7e",
+
+    storageBucket: "best-6dc7e.firebasestorage.app",
+
+    messagingSenderId: "790995369457",
+
+    appId: "1:790995369457:web:bafb5e41da8af071d9d850",
+
+    measurementId: "G-1BE86M53DW"
+};
+
+export default firebaseConfig;
